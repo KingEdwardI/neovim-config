cbe19e6d97e86641505bdeebcb509d1642260179
Eddie <edward.vetterdrake@alteryx.com>
2022-01-25 15:06:59 -0700

    adding invariant_require and implementing on impatient

--- init.lua
+++ init.lua
@@ -1,4 +1,15 @@
-require('impatient')
-require('keymappings')
+local utils = require('utils')
+local invariant_require = utils.invariant_require
+
+invariant_require('impatient')
+
+-- local present = pcall(require, 'impatient')
+
+-- if not present then
+--   print('not present')
+--   return false
+-- end
+
 require('plugins')
+require('keymappings')
 require('settings')
--- lua/utils.lua
+++ lua/utils.lua
@@ -8,4 +8,21 @@ utils.buf_map = function(bufnr, mode, lhs, rhs, opts)
   })
 end
 
+utils.invariant_require = function(pluginName)
+  local present, plugin = pcall(require, pluginName)
+
+  if not present then
+    vim.cmd([[
+    echohl ErrorMsg
+    echo "Error occured while loading a plugin"
+    echohl None
+    echo "Unable to load plugin: \"]] .. pluginName .. [[\"."
+    echo "Are you sure it is installed? Check :PackerStatus and run :PackerInstall if needed."
+    ]])
+    return false
+  end
+
+  return plugin
+end
+
 return utils
