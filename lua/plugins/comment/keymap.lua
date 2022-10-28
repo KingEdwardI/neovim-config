baa3ead5ed587498e4b8f0c317db6fb0da7998c8
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2022-10-28 21:15:56 +0000

    Fix issue where only a single line can be commented

--- lua/plugins/comment/keymap.lua
+++ lua/plugins/comment/keymap.lua
@@ -3,4 +3,4 @@ local set_keymap = utils.set_keymap
 
 -- Need to override the default mapping manually so that commenting works right and doesn't accidentally
 -- trigger `c` after commenting.
-set_keymap('v', ',cc', '<CMD>lua require("Comment.api").call("toggle_current_linewise_op")<CR>g@$')
+set_keymap('v', ',cc', '<PLug>(comment_toggle_linewise_visual)')
