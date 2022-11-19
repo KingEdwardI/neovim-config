3235b57646c41b5b88b3da866e31915683494f6b
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2022-11-19 02:00:55 +0000

    Implement alpha-nvim

---
+++ lua/plugins/alpha-nvim/config.lua
@@ -0,0 +1,274 @@
+local utils = require('utils')
+local invariant_require = utils.invariant_require
+local theme_utils = require('plugins.alpha-nvim.themes.utils')
+local mru = theme_utils.mru
+local alpha = invariant_require('alpha')
+local dashboard = invariant_require('alpha.themes.dashboard')
+
+vim.cmd([[
+  let s:chocobo_color1 = 0
+  let s:chocobo_color_2 = 0
+  let s:chocobo_color_3 = 0
+  let s:chocobo_color_4 = 0
+  let s:chocobo_color_5 = 0
+  let s:chocobo_color_6 = 0
+  let s:chocobo_color_7 = 0
+  let s:chocobo_color_8 = 0
+  let s:chocobo_color_9 = 0
+  let s:chocobo_color_10 = 0
+  let s:chocobo_color_11 = 0
+  let s:chocobo_color_12 = 0
+  let s:chocobo_color_13 = 0
+  let s:chocobo_color_14 = 0
+  let s:chocobo_color_15 = 0
+  let s:chocobo_color_16 = 0
+  let s:chocobo_color_17 = 0
+  let s:chocobo_color_18 = 0
+  let s:chocobo_color_19 = 0
+
+function HighlightChocobo()
+  highlight ChocoboGold guifg=#AB893F
+  highlight ChocoboYellow guifg=#E7BD3E
+  highlight ChocoboPaleYellow guifg=#C7B290
+  highlight ChocoboPurple guifg=#5E4683
+  highlight ChocoboBlue guifg=#6486AC
+  highlight ChocoboOrange guifg=#AB5729
+
+  let s:chocobo_color1 = matchadd('ChocoboYellow', '#')
+  let s:chocobo_color_2 = matchadd('ChocoboGold', '(')
+  let s:chocobo_color_3 = matchadd('ChocoboGold', '%')
+  let s:chocobo_color_4 = matchadd('ChocoboGold', '\.')
+  let s:chocobo_color_5 = matchadd('ChocoboYellow', '/#')
+  let s:chocobo_color_6 = matchadd('ChocoboYellow', '#\*')
+  let s:chocobo_color_7 = matchadd('ChocoboPaleYellow', '@')
+  let s:chocobo_color_8 = matchadd('ChocoboPurple', '/\*\*')
+  let s:chocobo_color_9 = matchadd('ChocoboBlue', '//')
+  let s:chocobo_color_10 = matchadd('ChocoboPurple', '\*\*')
+  let s:chocobo_color_11 = matchadd('ChocoboOrange', '/\\(')
+  let s:chocobo_color_12 = matchadd('ChocoboOrange', ' \*\*')
+  let s:chocobo_color_13 = matchadd('ChocoboOrange', '\*\* ')
+  let s:chocobo_color_14 = matchadd('ChocoboOrange', '/(')
+  let s:chocobo_color_15 = matchadd('ChocoboOrange', ' ///// ')
+  let s:chocobo_color_16 = matchadd('ChocoboOrange', '((//')
+  let s:chocobo_color_17 = matchadd('ChocoboOrange', '\*////')
+  let s:chocobo_color_18 = matchadd('ChocoboOrange', '//// ')
+  let s:chocobo_color_19 = matchadd('ChocoboOrange', '((//')
+endfunction
+
+function RemoveChocoboHighlight()
+  if &ft != 'alpha'
+    try
+      call matchdelete(s:chocobo_color1)     
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_2)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_3)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_4)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_5)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_6)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_7)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_8)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_9)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_10)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_11)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_12)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_13)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_14)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_15)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_16)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_17)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_18)
+    catch
+    endtry
+    try
+      call matchdelete(s:chocobo_color_19)
+    catch
+    endtry
+  endif
+endfunction
+
+function OpenAlpha()
+    let cnt = 0
+    for i in range(0, bufnr("$"))
+        if buflisted(i)
+         let cnt = cnt + 1
+        endif
+    endfor
+    if cnt <= 1
+      Alpha
+    endif
+endfunction
+
+function Echo()
+  echo "doin the thing"
+endfunction
+
+autocmd FileType alpha call HighlightChocobo()
+autocmd FileType * if &ft!="alpha" | call RemoveChocoboHighlight() | endif
+autocmd BufDelete * if !empty(&filetype) | execute OpenAlpha()
+]])
+
+local chocobo_banner = {
+  '                                                  ',
+  '                 #######((%%%%%%%%%.              ',
+  '            #####@@@@(((###%                      ',
+  '          ####/**//@@(((%#  #%                    ',
+  '          ####@@@**@@(((####                      ',
+  '          ##(((((((((#######                      ',
+  '          ((         %#%##              #%        ',
+  '                     ####(           %#*          ',
+  '                   #########     ####  /##        ',
+  '                 ######################*          ',
+  '                 #########((((#########*          ',
+  '                 **#####(#(((((((#(**             ',
+  '          @@     /(           (((((    (@@        ',
+  '            /////                  ((//           ',
+  '              *////            @@////             ',
+  '                                                  ',
+}
+
+local function info_text()
+  local total_plugins = #vim.tbl_keys(packer_plugins)
+  local datetime = os.date(' %d-%m-%Y')
+  local version = vim.version()
+  local nvim_version_info = '   v' .. version.major .. '.' .. version.minor .. '.' .. version.patch
+
+  return '        ' .. datetime .. '   ' .. total_plugins .. ' plugins' .. nvim_version_info
+end
+
+local info = {
+  type = 'text',
+  val = info_text(),
+  opts = {
+    hl = 'DevIconVim',
+    position = 'center',
+  },
+}
+
+local heading = {
+  type = 'text',
+  val = chocobo_banner,
+  opts = {
+    position = 'center',
+    hl = 'AlphaHeading',
+  },
+}
+
+local header = {
+  type = 'group',
+  val = {
+    heading,
+    { type = 'padding', val = 1 },
+    info,
+  },
+  opts = {
+    position = 'center',
+  },
+}
+
+local buttons = {
+  type = 'group',
+  val = {
+    dashboard.button('f', '  Find File', "<Cmd>lua require('fzf-lua').git_files()<CR>"),
+    dashboard.button('g', '  Live Grep', "<Cmd>lua require('fzf-lua').live_grep()<CR>"),
+    dashboard.button('t', '¤  Nvim Tree', '<Cmd>NvimTreeToggle<CR>'),
+    dashboard.button('u', '  Update plugins', '<Cmd>PackerSync<CR><Cmd>PackerCompile<CR>'),
+    dashboard.button('q', '✖  Quit NVIM', '<Cmd>qa<CR>'),
+  },
+  opts = {
+    position = 'center',
+    spacing = 1,
+  },
+}
+
+-- TODO: Fix the centering of the files so they are justified
+local recent_files = {
+  type = 'group',
+  val = {
+    {
+      type = 'text',
+      val = 'Recent files',
+      opts = {
+        hl = 'String',
+        shrink_margin = false,
+        position = 'center',
+      },
+    },
+    { type = 'padding', val = 1 },
+    {
+      type = 'group',
+      val = function()
+        return {
+          mru(1, vim.fn.getcwd(), 10),
+        }
+      end,
+      opts = { position = 'center' },
+    },
+  },
+  opts = {
+    position = 'center',
+  },
+}
+
+local config = {
+  layout = {
+    header,
+    { type = 'padding', val = 2 },
+    recent_files,
+    { type = 'padding', val = 3 },
+    buttons,
+  },
+}
+
+if alpha then
+  alpha.setup(config)
+end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,9 +45,7 @@ end
 
 packer.init({
   display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'rounded' })
-    end,
+    open_fn = function() return require('packer.util').float({ border = 'rounded' }) end,
   },
 })
 
@@ -89,9 +87,7 @@ return packer.startup(function(use)
   --]]
   use({
     'folke/lsp-colors.nvim',
-    config = function()
-      require('plugins.lsp-colors.config')
-    end,
+    config = function() require('plugins.lsp-colors.config') end,
   })
 
   --[[
@@ -122,9 +118,7 @@ return packer.startup(function(use)
       'molleweide/LuaSnip-snippets.nvim',
       -- 'rafamadriz/friendly-snippets',
     },
-    config = function()
-      require('plugins.nvim-cmp.config')
-    end,
+    config = function() require('plugins.nvim-cmp.config') end,
   })
   -- use('rafamadriz/friendly-snippets')
   use('molleweide/LuaSnip-snippets.nvim')
@@ -145,9 +139,7 @@ return packer.startup(function(use)
   use({
     'SirVer/ultisnips',
     requires = 'honza/vim-snippets',
-    config = function()
-      require('plugins.ultisnips.config')
-    end,
+    config = function() require('plugins.ultisnips.config') end,
   })
 
   --[[
@@ -158,9 +150,7 @@ return packer.startup(function(use)
   use({
     'metalelf0/jellybeans-nvim',
     requires = 'rktjmp/lush.nvim',
-    config = function()
-      require('colorscheme')
-    end,
+    config = function() require('colorscheme') end,
   })
 
   --[[
@@ -201,9 +191,7 @@ return packer.startup(function(use)
   --]]
   use({
     'numToStr/Comment.nvim',
-    config = function()
-      require('plugins.comment.config')
-    end,
+    config = function() require('plugins.comment.config') end,
   })
 
   --[[
@@ -233,9 +221,7 @@ return packer.startup(function(use)
     requires = 'vijaymarupudi/nvim-fzf',
     dir = '~/.fzf',
     run = './install --all',
-    config = function()
-      require('plugins.fzf.config')
-    end,
+    config = function() require('plugins.fzf.config') end,
   })
   use({
     'ibhagwan/fzf-lua',
@@ -251,9 +237,7 @@ return packer.startup(function(use)
   --]]
   use({
     'windwp/nvim-autopairs',
-    config = function()
-      require('plugins.auto-pairs.config')
-    end,
+    config = function() require('plugins.auto-pairs.config') end,
   })
 
   --[[
@@ -268,10 +252,6 @@ return packer.startup(function(use)
   --]
   -- use('liuchengxu/vim-which-key')
 
-  --[[
-  -- which-key.nvim - display available keybindings in a popup
-  -- https://github.com/folke/which-key.nvim
-  --]]
   use({
     'folke/which-key.nvim',
     config = function()
@@ -280,14 +260,22 @@ return packer.startup(function(use)
   })
 
   --[[
+  -- alpha-nvim - fancy dashboard on startup
+  -- https://github.com/goolord/alpha-nvim
+  --]]
+  use({
+    -- Temporarily using my fork of the plugin because otherwise it breaks.
+    'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function() require('plugins.alpha-nvim.config') end,
+  })
+  --[[
   -- nvim-web-devicons - neato icons
   -- https://github.com/kyazdani42/nvim-web-devicons
   --]]
   use({
     'kyazdani42/nvim-web-devicons',
-    config = function()
-      require('plugins.nvim-devicons.config')
-    end,
+    config = function() require('plugins.nvim-devicons.config') end,
   })
 
   --[[
@@ -297,9 +285,7 @@ return packer.startup(function(use)
   use({
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-      require('plugins.nvim-tree.config')
-    end,
+    config = function() require('plugins.nvim-tree.config') end,
   })
 
   --[[
@@ -309,9 +295,7 @@ return packer.startup(function(use)
   use({
     'hoob3rt/lualine.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-      require('plugins.lualine.config')
-    end,
+    config = function() require('plugins.lualine.config') end,
   })
 
   --[[
@@ -321,9 +305,7 @@ return packer.startup(function(use)
   use({
     'romgrk/barbar.nvim',
     requires = { 'kyazdani42/nvim-web-devicons' },
-    config = function()
-      require('plugins.barbar.config')
-    end,
+    config = function() require('plugins.barbar.config') end,
   })
 
   --[[
@@ -333,9 +315,7 @@ return packer.startup(function(use)
   use({
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
-    config = function()
-      require('plugins.treesitter.config')
-    end,
+    config = function() require('plugins.treesitter.config') end,
   })
 
   --[[
@@ -345,9 +325,7 @@ return packer.startup(function(use)
   --
   use({
     'ludovicchabant/vim-gutentags',
-    config = function()
-      require('plugins.vim-gutentags.config')
-    end,
+    config = function() require('plugins.vim-gutentags.config') end,
   })
 
   --[[
@@ -367,9 +345,7 @@ return packer.startup(function(use)
   --]]
   use({
     't9md/vim-choosewin',
-    config = function()
-      require('plugins.choosewin.config')
-    end,
+    config = function() require('plugins.choosewin.config') end,
   })
 
   --[[
@@ -378,9 +354,7 @@ return packer.startup(function(use)
   --]]
   use({
     'folke/trouble.nvim',
-    config = function()
-      require('plugins.trouble.config')
-    end,
+    config = function() require('plugins.trouble.config') end,
   })
 
   --[[
@@ -409,9 +383,7 @@ return packer.startup(function(use)
     'tami5/lspsaga.nvim',
     opt = true,
     requires = 'neovim/nvim-lspconfig',
-    config = function()
-      require('plugins.lspsaga.config')
-    end,
+    config = function() require('plugins.lspsaga.config') end,
     cmd = { 'Lspsaga' },
     ft = {
       'javascript',
@@ -430,9 +402,7 @@ return packer.startup(function(use)
   --]]
   use({
     'kosayoda/nvim-lightbulb',
-    config = function()
-      require('plugins.nvim-lightbulb.config')
-    end,
+    config = function() require('plugins.nvim-lightbulb.config') end,
   })
 
   --[[
@@ -441,16 +411,12 @@ return packer.startup(function(use)
   --]]
   use({
     'lukas-reineke/indent-blankline.nvim',
-    config = function()
-      require('plugins.indent-blankline.config')
-    end,
+    config = function() require('plugins.indent-blankline.config') end,
   })
 
   use({
     'anuvyklack/pretty-fold.nvim',
-    config = function()
-      require('plugins.pretty-fold.config')
-    end,
+    config = function() require('plugins.pretty-fold.config') end,
   })
 
   -- EndSection - General -------------------------------------------------------------------------------------
@@ -465,9 +431,7 @@ return packer.startup(function(use)
     'itspriddle/vim-shellcheck',
     opt = true,
     ft = { 'sh', 'bash', 'zsh' },
-    config = function()
-      require('plugins.shellcheck.config')
-    end,
+    config = function() require('plugins.shellcheck.config') end,
   })
 
   -- EndSection - Bash -------------------------------------------------------------------------------------
@@ -482,9 +446,7 @@ return packer.startup(function(use)
     'rhysd/vim-fixjson',
     opt = true,
     ft = { 'json', 'json5' },
-    config = function()
-      require('plugins.fix-json.config')
-    end,
+    config = function() require('plugins.fix-json.config') end,
   })
 
   --[[
@@ -519,9 +481,7 @@ return packer.startup(function(use)
     opt = true,
     ft = { 'markdown' },
     run = 'cd app && yarn install',
-    config = function()
-      require('plugins.markdown-preview.config')
-    end,
+    config = function() require('plugins.markdown-preview.config') end,
   })
 
   --[[
@@ -563,9 +523,7 @@ return packer.startup(function(use)
   --]]
   use({
     'kdheepak/lazygit.nvim',
-    config = function()
-      require('plugins.lazygit.config')
-    end,
+    config = function() require('plugins.lazygit.config') end,
   })
 
   --[[
@@ -582,9 +540,7 @@ return packer.startup(function(use)
     'f-person/git-blame.nvim',
     opt = true,
     cmd = { 'GitBlameToggle' },
-    config = function()
-      require('plugins.git-blame.config')
-    end,
+    config = function() require('plugins.git-blame.config') end,
   })
 
   --[[
@@ -596,9 +552,7 @@ return packer.startup(function(use)
     requires = 'nvim-lua/plenary.nvim',
     opt = true,
     cmd = { 'DiffviewOpen', 'DiffviewClose' },
-    config = function()
-      require('plugins.diff-view.config')
-    end,
+    config = function() require('plugins.diff-view.config') end,
   })
 
   --[[
@@ -607,9 +561,8 @@ return packer.startup(function(use)
   --]]
   use({
     'akinsho/git-conflict.nvim',
-    config = function()
-      require('git-conflict').setup()
-    end,
+    opt = true,
+    config = function() require('git-conflict').setup() end,
   })
 
   -- EndSection - Git -------------------------------------------------------------------------------------
@@ -624,7 +577,5 @@ return packer.startup(function(use)
 
   -- EndSection - Debugging -------------------------------------------------------------------------------------
 
-  if PackerBootstrap then
-    require('packer').sync()
-  end
+  if PackerBootstrap then require('packer').sync() end
 end)
