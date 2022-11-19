d7b038434ad76c77190d571820a32361c171f57a
Eddie <edward.vetterdrake@alteryx.com>
2022-11-18 18:43:10 -0700

    making pretty-fold better

--- lua/plugins/pretty-fold/config.lua
+++ lua/plugins/pretty-fold/config.lua
@@ -6,5 +6,15 @@ local pretty_fold = invariant_require('pretty-fold')
 if pretty_fold then
   pretty_fold.setup({
     fill_char = '.',
+    sections = {
+      left = {
+        '+',
+        function() return string.rep('-', vim.v.foldlevel) end,
+        ' ',
+        'number_of_folded_lines',
+        ':',
+        'content',
+      },
+    },
   })
 end
